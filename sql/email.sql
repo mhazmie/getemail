8251ee0fe7fe993bd14c00bac1c09900ae31ba27
MH <mhazmie96@gmail.com>
2025-06-10 13:50:01 +0800

    1.2

--- sql/email.sql
+++ sql/email.sql
@@ -1,12 +1,10 @@
-
 DROP TABLE IF EXISTS `email`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `email` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1071 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+)
 
