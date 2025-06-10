59860486d7d564b18b6b5a8ffc2ef38e55489b57
MH <mhazmie96@gmail.com>
2025-06-10 13:21:41 +0800

    1.0

--- app.js
+++ app.js
@@ -9,22 +9,38 @@ var connection = mysql.createConnection({
   database: 'node'
 });
 
+var bodyParser = require('body-parser');
+
 const port = process.env.PORT || 5000;
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extend: true}));
+
+app.use(express.static(__dirname +'/styles'))
+
 app.listen(port, function () {
-  console.log("Server Has Started!");
+  console.log('Server Has Started!');
 });
 
-// app.get("/", function(req, res){
-//     res.send("HELLO FROM OUR WEB APP!");
-// });
-
-app.get("/", function(req, res){
-    var q = 'SELECT COUNT(*) AS count FROM email';
-    connection.query(q, function(error, results, fields)
+app.get('/', function(req, res){
+  var q = 'SELECT COUNT(*) AS count FROM email';
+  connection.query(q, function(error, results, fields)
     {if (error) throw error;
-        console.log(results[0].count);
-        var nou = results[0].count;
-        res.send("We have " + nou + " users");
-    });
+      console.log(results[0].count);
+      var nou = results[0].count;
+      res.render('home', {nou:nou});
+    }
+  );
+});
+
+app.post('/register', function(req, res){
+  var q = ('INSERT INTO email SET ?');
+  var person = {email: req.body.email};
+  connection.query(q, person, function(error, results)
+    {if (error) throw error;
+      console.log(results);
+      res.redirect('/');
+    }
+  );
 });
