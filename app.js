e6d38a199ea238eb70c9408d1ad8100fab14994a
MH <mhazmie96@gmail.com>
2025-06-12 16:07:53 +0800

    v2.0

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -7,7 +8,6 @@ var connection = mysql.createConnection({
   password: 'rootpassword',
   database: 'node'
 });
-var bodyParser = require('body-parser');
 
 const port = 5000;
 const domain = 'http://localhost:';
@@ -25,43 +25,41 @@ app.get('/', function(req, res){
   var q = 'SELECT COUNT(*) AS count FROM email';
   connection.query(q, function(error, results, fields)
     {if (error) throw error;
-      // console.log(results[0].count);
       var nou = results[0].count;
       res.render('home', {nou:nou});
     }
   );
 });
 
-// app.get('/list', function(req, res){
-//   var q = 'SELECT email AS list FROM email';
-//   connection.query(q, function(error, results, fields)
-//     {if (error) throw error;
-//       var list = results.list;
-//       res.render('list', {list:list});
-//     }
-//   );
-// })
-
 app.get('/list', function(req, res) {
   var page = parseInt(req.query.page) || 1;
   var limit = 20;
   var offset = (page - 1) * limit;
+  var search = req.query.search || '';
 
-  var q = 'SELECT COUNT(*) AS count FROM email';
-  var w = 'SELECT id, email FROM email LIMIT ? OFFSET ?';
-  // var w = 'SELECT id, email FROM email';
+  let countQuery, dataQuery, queryParams;
 
-  connection.query(q, function(error, countResult) {
-    if (error) throw error;
-    var totalEmails = countResult[0].count;
-    var totalPages = Math.ceil(totalEmails / limit);
+  if (search) {
+    q = 'SELECT COUNT(*) AS count FROM email WHERE email LIKE ?';
+    w = 'SELECT id, email FROM email WHERE email LIKE ? LIMIT ? OFFSET ?';
+    r = [`%${search}%`, limit, offset];
+  } else {
+    q = 'SELECT COUNT(*) AS count FROM email';
+    w = 'SELECT id, email FROM email LIMIT ? OFFSET ?';
+    r = [limit, offset];
+  }
 
-    connection.query(w, [limit, offset], function(error, results) {
+  connection.query(q, search ? [`%${search}%`] : [], function(error, countResult) {
+  if (error) throw error;
+  const totalEmails = countResult[0].count;
+  const totalPages = Math.ceil(totalEmails / limit);
+    connection.query(w, r, function(error, results) {
       if (error) throw error;
       res.render('list', {
         list: results,
         currentPage: page,
-        totalPages: totalPages
+        totalPages: totalPages,
+        search: search
       });
     });
   });
